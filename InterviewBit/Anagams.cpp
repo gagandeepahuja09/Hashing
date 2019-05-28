cf172f17c7ce0b440bd6a2d7be51d97bdebe1c48
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-05-28 14:58:09 +0530

    easy optimized n2logn to n2

--- InterviewBit/Anagams.cpp
+++ InterviewBit/Anagams.cpp
@@ -1,16 +1,35 @@
+/*
+Given an array of strings, return all groups of strings that are anagrams. Represent a group by a list of integers representing the index in the original list. Look at the sample case for clarification.
+
+    Anagram : a word, phrase, or name formed by rearranging the letters of another, such as 'spar', formed from 'rasp'
+
+    Note: All inputs will be in lower-case. 
+
+Example :
+
+Input : cat dog god tca
+Output : [[1, 4], [2, 3]]
+
+*/
+
 vector<vector<int> > Solution::anagrams(const vector<string> &A) {
     map<string, vector<int>> mp;
     for(int i = 0; i < A.size(); i++) {
-        string temp = A[i];
-        sort(temp.begin(), temp.end());
-        mp[temp].push_back(i + 1);
+        vector<int> cnt(26, 0);
+        for(int j = 0; j < A[i].size(); j++) {
+            cnt[A[i][j] - 'a']++;
+        }
+        string s = "";
+        for(int j = 0; j < 26; j++) {
+            s += (cnt[j] + '.'); 
+        }
+        mp[s].push_back(i + 1);
     }
-    vector<vector<int>> res;
-    auto it = mp.begin();
-    while(it != mp.end()) {
-        res.push_back(it -> second);
-        it++;
+    vector<vector<int>> ret;
+    for(auto it = mp.begin(); it != mp.end(); it++) {
+        vector<int> v = it -> second;
+        ret.push_back(v);
     }
-    return res;
+    return ret;
 }
 
