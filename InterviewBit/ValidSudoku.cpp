e5261780b0f7c733722d1714c8d87548cdb62624
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-06-20 10:57:44 +0530

    easy

--- InterviewBit/ValidSudoku.cpp
+++ InterviewBit/ValidSudoku.cpp
@@ -1,17 +1,21 @@
 int Solution::isValidSudoku(const vector<string> &A) {
-    vector<vector<int>> row(10, vector<int>(10, 0));
-    vector<vector<int>> col(10, vector<int>(10, 0));
-    vector<vector<vector<int>>> square(3, vector<vector<int>>(3, vector<int>(10, 0)));
+    int row[10][10];
+    memset(row, 0, sizeof row);
+    int col[10][10];
+    memset(col, 0, sizeof col);
+    int square[3][3][10];
+    memset(square, 0, sizeof square);
     for(int i = 0; i < A.size(); i++) {
         for(int j = 0; j < A[0].size(); j++) {
-            if(A[i][j] != '.') {
-                int val = A[i][j] - '0';
-                if(val < 0 || val > 10 || row[val][i] || col[val][j] || square[i / 3][j / 3][val])
-                    return false;
-                row[val][i] = 1;
-                col[val][j] = 1;
-                square[i / 3][j / 3][val] = 1;
+            if(A[i][j] == '.')
+                continue;
+            int num = A[i][j] - '0';
+            if(row[i][num] == 1 || col[j][num] == 1 || square[i / 3][j / 3][num] == 1) {
+                return false;    
             }
+            row[i][num] = 1;
+            col[j][num] = 1;
+            square[i / 3][j / 3][num] = 1;
         }
     }
     return true;
