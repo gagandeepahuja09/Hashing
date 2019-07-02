a858eb089027244504bd3551baae982cdb812a99
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-02 20:14:04 +0530

    very good question

---
+++ LeetCode/NoOfSubmatricesThatSumToTarget.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numSubmatrixSumTarget(vector<vector<int>>& A, int B) {
+        int m = A.size(), n = A[0].size();
+        for(int i = 0; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                A[i][j] += A[i][j - 1];    
+            }
+        } 
+        int ret = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                unordered_map<int, int> count;
+                count[0] = 1;
+                int curr = 0;
+                for(int k = 0; k < m; k++) {
+                    curr += A[k][j] - (i > 0 ? A[k][i - 1] : 0);
+                    ret += count[curr - B];
+                    count[curr]++;
+                }
+            }
+        }
+        return ret;
+    }
+};
