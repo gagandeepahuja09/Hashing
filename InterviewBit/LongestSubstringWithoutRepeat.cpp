28a1f8daa2837e43b2f807c0f8f7f30f45da2f45
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-23 21:59:11 +0530

    important question

--- InterviewBit/LongestSubstringWithoutRepeat.cpp
+++ InterviewBit/LongestSubstringWithoutRepeat.cpp
@@ -1,13 +1,11 @@
 int Solution::lengthOfLongestSubstring(string A) {
-    int start = -1, maxLen = 1;
     vector<int> pos(256, -1);
+    int mx = 0, start = -1;
     for(int i = 0; i < A.size(); i++) {
-        if(pos[A[i]] > start) {
-            start = pos[A[i]];
-        }
-        maxLen = max(maxLen, i - start);
+        start = max(start, pos[A[i]]);
+        mx = max(mx, i - start);
         pos[A[i]] = i;
     }
-    return maxLen;
+    return mx;
 }
 
