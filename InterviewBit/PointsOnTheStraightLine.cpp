1b1c68318add8cca99f08f8029d7a3ef31e10173
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-24 11:09:22 +0530

    imp que

--- InterviewBit/PointsOnTheStraightLine.cpp
+++ InterviewBit/PointsOnTheStraightLine.cpp
@@ -1,32 +1,22 @@
 int Solution::maxPoints(vector<int> &A, vector<int> &B) {
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    unordered_map<double, int> mp;
-    if(!A.size())
-        return 0;
-    int mx = 1;
-    int currMax = 0;
-    int duplicate, vertical;
-    for(int i=0; i<A.size(); i++) {
-        currMax = 0, duplicate = 1, vertical = 0;
-        double slope = 0.0;
-        for(int j=i+1; j<A.size(); j++) {
-            if(A[j] != A[i]) {
-                slope = (1.0 * (B[j] - B[i])/(A[j] - A[i]));
-                mp[slope]++;
-            }
+    int mx = 0;
+    for(int i = 0; i < A.size(); i++) {
+        unordered_map<string, int> mp;
+        int duplicates = 1;
+        for(int j = i + 1; j < A.size(); j++) {
+            if(A[i] == A[j] && B[i] == B[j])
+                duplicates++;
             else {
-                if(B[i] == B[j])
-                    duplicate++;
-                else
-                    vertical++;
-            }
-            currMax = max(currMax, max(vertical, mp[slope]));
+                int dx = A[j] - A[i], dy = B[j] - B[i];
+                int g = __gcd(dx, dy);
+                string s = to_string(dx / g) + '_' + to_string(dy / g);
+                mp[s]++;
+            }    
         }
-        mx = max(mx, currMax + duplicate);
-        mp.clear();
+        mx = max(mx, duplicates);
+        for(auto it = mp.begin(); it != mp.end(); it++) {
+            mx = max(mx, it -> second + duplicates);
+        }
     }
     return mx;
 }
