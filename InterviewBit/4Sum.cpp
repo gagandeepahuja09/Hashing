fdc15ff749fc9493220342b3099c273ef7e087c5
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-24 21:20:17 +0530

    easy

--- InterviewBit/4Sum.cpp
+++ InterviewBit/4Sum.cpp
@@ -1,40 +1,23 @@
 vector<vector<int> > Solution::fourSum(vector<int> &A, int B) {
-    vector<vector<int>> res;
-    set<vector<int>> st;
-    map<int, vector<pair<int, int>>> mp;
-    for(int i=0; i<A.size(); i++) {
-        for(int j = i + 1; j<A.size(); j++) {
-            mp[A[i] + A[j]].push_back({i, j});
-        }
-    }
-    
-    while(mp.size() > 0) {
-        auto it = mp.begin();
-        int temp = it -> first;
-        if(mp.find(B - temp) != mp.end()) {
-            for(int i=0; i < mp[temp].size(); i++) {
-                for(int j=0; j<mp[B - temp].size(); j++) {
-                    int i1 = (mp[temp][i]).first;
-                    int j1 = (mp[temp][i]).second;
-                    int i2 = (mp[B - temp][j]).first;
-                    int j2 = (mp[B - temp][j]).second;
-                    if(i1 != i2 && i1 != j2 && j1 != i2 && j1 != j2) {
-                        vector<int> v;
-                        v.push_back(A[i1]); v.push_back(A[i2]); 
-                        v.push_back(A[j1]); v.push_back(A[j2]); 
-                        sort(v.begin(), v.end());
-                        st.insert(v);
+    unordered_map<int, vector<pair<int, int>>> mp;
+    set<vector<int>> ret;
+    for(int i = 0; i < A.size(); i++) {
+        for(int j = i + 1; j < A.size(); j++) {
+            int target = B - A[i] - A[j];
+            if(mp.find(target) != mp.end()) {
+                vector<pair<int, int>> v = mp[target];
+                for(int k = 0; k < v.size(); k++) {
+                    int a = i, b = j, c = v[k].first, 
+                    d = v[k].second;
+                    if(a != b && a != c && a != d && b != c && b != d && c != d) {
+                        vector<int> temp = { A[a], A[b], A[c], A[d] };
+                        sort(temp.begin(), temp.end());
+                        ret.insert(temp);
                     }
                 }
             }
+            mp[A[i] + A[j]].push_back({ i, j });
         }
-        mp.erase(it);
     }
-    auto it = st.begin();
-    while(it != st.end()) {
-        res.push_back(*it);
-        it++;
-    }
-    return res;
+    return vector<vector<int>>(ret.begin(), ret.end());
 }
-
