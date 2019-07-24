259351a846df3e728ddc3b8a0b580be4e810ee53
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-24 21:52:14 +0530

    easy

--- InterviewBit/ColorfulNumber.cpp
+++ InterviewBit/ColorfulNumber.cpp
@@ -1,19 +1,17 @@
-#define ll long long int
-
 int Solution::colorful(int A) {
     vector<int> v;
+    unordered_map<int, int> mp;
     while(A) {
-        v.push_back(A%10);
+        v.push_back(A % 10);
         A /= 10;
     }
-    unordered_map<ll, ll> mp;
-    for(int i=0; i<v.size(); i++) {
-        ll prod = 1;
-        for(int j=i; j<v.size(); j++) {
-            prod = prod * v[j];
+    for(int i = 0; i < v.size(); i++) {
+        int prod = 1;
+        for(int j = i; j < v.size(); j++) {
+            prod *= v[j];
             if(mp.find(prod) != mp.end())
                 return 0;
-            mp[prod] = 1;    
+            mp[prod]++;    
         }
     }
     return 1;
