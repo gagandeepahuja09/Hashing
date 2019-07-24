3c3a5c814b575a516221d744128e0a2b5adebd93
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-24 19:08:02 +0530

    easy

--- InterviewBit/Equal.cpp
+++ InterviewBit/Equal.cpp
@@ -1,27 +1,20 @@
-/*Given an array A of integers, find the index of values that satisfy A + B = C + D, where
-A,B,C & D are integers values in the array*/
-
-#define ll long long int
-
 vector<int> Solution::equal(vector<int> &A) {
-    map<ll, pair<int, int>> mp;
-    vector<int> res;
+    unordered_map<int, pair<int, int>> mp;
+    vector<int> ret;
     for(int i = 0; i < A.size(); i++) {
         for(int j = i + 1; j < A.size(); j++) {
-            if(mp.find(A[i] + A[j]) != mp.end()) {
-                if(mp[A[i] + A[j]].first != i && mp[A[i] + A[j]].second != i && 
-                mp[A[i] + A[j]].first != j && mp[A[i] + A[j]].second != j) {
-                    vector<int> v = 
-                    { mp[A[i] + A[j]].first, mp[A[i] + A[j]].second, i, j };
-                    if(!res.size() || v < res)
-                        res = v;
+            int sum = A[i] + A[j];
+            if(mp.find(sum) != mp.end()) {
+                int x = mp[sum].first, y = mp[sum].second;
+                if(x != i && x != j && x != y && y != i && y != j) {
+                    vector<int> v = { x, y, i, j };
+                    if(ret.size() == 0 || v < ret)
+                        ret = v;
                 }
             }
-            else {
-                mp[A[i] + A[j]] = { i, j };
-            }
+            else
+                mp[sum] = { i, j };
         }
     }
-    return res;
+    return vector<int>(ret.begin(), ret.end());
 }
-
