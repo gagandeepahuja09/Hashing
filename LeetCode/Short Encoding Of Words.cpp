af77b614e5a28dc9e10708632cf07c8ba0751039
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-10-25 23:32:23 +0530

    very good question IMP

---
+++ LeetCode/Short Encoding Of Words.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumLengthEncoding(vector<string>& words) {
+        unordered_set<string> s(words.begin(), words.end());
+        //n * k * k
+        for(string w : s) {
+            for(int i = 1; i < w.size(); i++) {
+                s.erase(w.substr(i));
+            }
+        }
+        int ans = 0;
+        for(string w : s) {
+            ans += (w.size() + 1);
+        }
+        return ans;
+    }
+};
