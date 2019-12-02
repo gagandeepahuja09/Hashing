be4b815313c04c7732ff027c40d3e2b53ecb4ce5
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-12-02 17:03:17 +0530

    easy

---
+++ LeetCode/4 Sum II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        unordered_map<int, int> mp;
+        for(int i = 0; i < A.size(); i++) {
+            for(int j = 0; j < B.size(); j++) {
+                mp[A[i] + B[j]]++;
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < C.size(); i++) {
+            for(int j = 0; j < D.size(); j++) {
+                ans += mp[-(C[i] + D[j])];
+            }
+        }
+        return ans;
+    }
+};
